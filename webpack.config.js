c9d91583e2e353d1e60570a569c94f51ed0bcc95
zhangchengcheng <zhangchengcheng@bonc.com.cn>
2017-09-30 23:02:11 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+var path=require('path');
+var webpack=require('webpack');
+var HtmlWebpackPlugin=require('html-webpack-plugin');
+var OpenBrowserPlugin=require('open-browser-webpack-plugin');
+
+module.export={
+    
+};
